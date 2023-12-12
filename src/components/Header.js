0d77c455a0a863bf934f7548d37d05fbbbee1804
Wesam Abadi <wisamabady7@gmail.com>
2023-12-12 19:28:10 +0100

    style & header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,26 @@ import React from "react";
 import { IoLogoGithub } from "react-icons/io";
 import { SiGmail } from "react-icons/si";
 import { FaLinkedinIn } from "react-icons/fa6";
+import "../styles/Header.scss";
 
 function Header() {
   return (
-    <div>
-      <IoLogoGithub />
-      <SiGmail />
-      <FaLinkedinIn />
+    <div className="header">
+      <a href="https://github.com/WesamAbadi">
+        <div>
+          <IoLogoGithub />
+        </div>
+      </a>
+      <a href="mailto:wisamabady7@gmail.com">
+        <div>
+          <SiGmail />
+        </div>
+      </a>
+      <a href="https://www.linkedin.com/in/wesamabadi/">
+        <div>
+          <FaLinkedinIn />
+        </div>
+      </a>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,52 +1,77 @@
 import React from "react";
 import { IoLogoGithub } from "react-icons/io";
+import { SiSvelte } from "react-icons/si";
+import { FaReact, FaSass } from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
 
 function Home() {
   return (
     <div className="hero">
       <div className="container">
         <div className="row">
-          <div className="col-6 hero-text">
+          <div className="col-5 hero-text">
             <div>
               <h2>Hi, I'm</h2>
               <h1>Wesam Abadi</h1>
               <p>Front End Developer</p>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-7">
+            <div className="projects-title">
+              <h2>Projects</h2>
+            </div>
             <div className="projects">
               <div className="project">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://language.wesamabadi.com/"
+                  href="https://language.wesamabadi.com"
                 >
-                  <h2>Language Master</h2>
-                  <p>Description of project 1</p>
+                  <div className="project-div">
+                    <h3>Language Master</h3>
+                    <p>
+                      Language Learning App With Interactive Lessons & Real-time
+                      Progress Tracking.
+                    </p>
+                    <div className="stack">
+                      <FaReact />
+                      <IoLogoFirebase />
+                      <FaSass />
+                    </div>
+                  </div>
                 </a>
               </div>
               <div className="project">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://cinema.wesamabadi.com/"
+                  href="https://moviemate.wesamabadi.com"
                 >
-                  <h2>Movie Mate</h2>
-                  <p>Description of project 2</p>
+                  <div className="project-div">
+                    <h3>Movie Mate</h3>
+                    <p>Movie streaming & rating site.</p>
+                    <div className="stack">
+                      <SiSvelte />
+                    </div>
+                  </div>
                 </a>
               </div>
 
               <div target="_blank" rel="noreferrer" className="project">
-                <a href="https://wesamabadi.com/">
-                  <h2>Project 3</h2>
-                  <p>Description of project 3</p>
+                <a href="https://wesamabadi.com">
+                  <div className="project-div">
+                    <h3>Project 3</h3>
+                    <p>Description of project 3</p>
+                  </div>
                 </a>
               </div>
               <div target="_blank" rel="noreferrer" className="project">
-                <a href="https://wesamabadi.com/">
-                  <h2>
-                    More on <IoLogoGithub />
-                  </h2>
+                <a href="https://github.com/WesamAbadi">
+                  <div className="project-div">
+                    <h3>
+                      More on <IoLogoGithub />
+                    </h3>
+                  </div>
                 </a>
               </div>
             </div>
